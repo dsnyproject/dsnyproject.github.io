37f42a3aaa0c2b8f5d01621ba4b7d2224a8de268
Farhan <fmshyder@gmail.com>
2019-12-14 23:05:30 -0500

    Updates

---
+++ precache-manifest.9087bec021ebd12b480936b742033512.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cda42e487c49704ab291e21a3c08e7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f2766773344e48467b0",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "ac33eec1fc54704678e1",
+    "url": "/static/js/2.c24a92db.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c24a92db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f2766773344e48467b0",
+    "url": "/static/js/main.0df11336.chunk.js"
+  },
+  {
+    "revision": "efdfa130959840238bf6",
+    "url": "/static/js/runtime-main.0959cebc.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
